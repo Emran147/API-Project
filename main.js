cd393fe851ed8fd9fd34c7525f6f77fd66710869
Emran147 <emran.qedan11@hotmail.com>
2023-12-12 15:18:24 +0200

    More organization and arrangement

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const dataArrange = new dataManager
 const urlManager = new urls
+const apiManager = new ApiManager(urlManager.getUserURL(),urlManager.getQuoteURL(),urlManager.getPokemonURL(),urlManager.getBaconURL())
+
+
 
 const refresh = function(){
-  const apiManager = new ApiManager(urlManager.getUserURL(),urlManager.getQuoteURL(),urlManager.getPokemonURL(),urlManager.getBaconURL())
   Promise.all([apiManager.userDataFunc(), apiManager.quoteDataFunc(),apiManager.pokemonDataFunc(),apiManager.baconDataFunc()])
   .then(([userData, quoteData ,pokemonData,baconData]) => {
     let dataObj =  dataArrange.createDataObj(userData, quoteData ,pokemonData,baconData)
